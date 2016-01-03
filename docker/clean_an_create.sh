584b8b76e74feb3cc340ee6cc3f08f8e797d9433
xdoo <claus.straube@gmail.com>
2016-01-03 12:32:46 +0100

    Config Service Konfiguration abgeschlossen.

--- docker/clean_an_create.sh
+++ docker/clean_an_create.sh
@@ -5,6 +5,7 @@ docker rm $(docker ps -a -q)
 docker rmi docker_simplesubservice
 docker rmi docker_simpleservice
 docker rmi docker_eureka
+docker rmi docker_config
 
 # build images and start
 docker-compose up -d
