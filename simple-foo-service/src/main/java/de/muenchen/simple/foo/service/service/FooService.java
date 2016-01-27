0ff13bf2a125bfeee985afff0b5fd92b470dd977
xdoo <claus.straube@gmail.com>
2016-01-27 21:31:31 +0100

    fixed configuration (load balancing does not work)

--- simple-foo-service/src/main/java/de/muenchen/simple/foo/service/service/FooService.java
+++ simple-foo-service/src/main/java/de/muenchen/simple/foo/service/service/FooService.java
@@ -4,6 +4,8 @@ import de.muenchen.simple.foo.service.domain.Foo;
 import de.muenchen.simple.foo.service.domain.FooRepository;
 import java.math.BigInteger;
 import java.security.SecureRandom;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,8 @@ public class FooService {
     private final FooRepository repo;
     
     private final SecureRandom random = new SecureRandom();
+    
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     public FooService(FooRepository repo) {
@@ -26,10 +30,12 @@ public class FooService {
     
     public Long saveBar() {
         String bar = new BigInteger(130, random).toString(32);
+        this.log.info(String.format("saving hash '%s'", bar));
         return this.repo.save(new Foo(bar)).getId();
     }
     
     public String loadBar(Long id) {
+        this.log.info(String.format("loading hash for is '%s'", id));
         return this.repo.findOne(id).getBar();
     }
     
